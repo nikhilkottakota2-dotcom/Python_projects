2c3db7925456de9e936b8dcc90858e9114c04cdc
nikhilkottakota2-dotcom <nikhilkottakota2@2gmail.com>
2026-07-12 17:11:46 +0530

    The last commit

--- pingpong/ponggame.py
+++ pingpong/ponggame.py
@@ -15,15 +15,66 @@ ball_obj = ball()
 
 scoreboard = board()
 
+r_up_pressed = False
+r_down_pressed = False
+l_up_pressed = False
+l_down_pressed = False
+
+def r_up_press():
+    global r_up_pressed
+    r_up_pressed = True
+
+def r_down_press():
+    global r_down_pressed
+    r_down_pressed = True
+
+def l_up_press():
+    global l_up_pressed
+    l_up_pressed = True
+
+def l_down_press():
+    global l_down_pressed
+    l_down_pressed = True
+
+def r_up_release():
+    global r_up_pressed
+    r_up_pressed = False
+
+def r_down_release():
+    global r_down_pressed
+    r_down_pressed = False
+
+def l_up_release():
+    global l_up_pressed
+    l_up_pressed = False
+
+def l_down_release():
+    global l_down_pressed
+    l_down_pressed = False
+
+screen.onkeypress(r_up_press, "Up")
+screen.onkeypress(r_down_press, "Down")
+screen.onkeypress(l_up_press, "w")
+screen.onkeypress(l_down_press, "s")
+
+screen.onkeyrelease(r_up_release, "Up")
+screen.onkeyrelease(r_down_release, "Down")
+screen.onkeyrelease(l_up_release, "w")
+screen.onkeyrelease(l_down_release, "s")
+
 screen.listen()
-screen.onkey(r_paddle.go_up, "Up")
-screen.onkey(r_paddle.go_down, "Down")
-screen.onkey(l_paddle.go_up, "w")
-screen.onkey(l_paddle.go_down, "s")
 
 game_on = True
 while game_on:
     time.sleep(0.05)
+    if r_up_pressed:
+        r_paddle.go_up()
+    if r_down_pressed:
+        r_paddle.go_down()
+    if l_up_pressed:
+        l_paddle.go_up()
+    if l_down_pressed:
+        l_paddle.go_down()
     ball_obj.move()
 
     if ball_obj.ycor() >= 280:
