e1aedc0e616a2c29069ad1c2d591514d2181c17f
nikhilkottakota2-dotcom <nikhilkottakota2@2gmail.com>
2026-07-21 22:02:51 +0530

    The csv and pandas worksheet

--- CSV_worksheet.py
+++ CSV_worksheet.py
@@ -1,11 +1,56 @@
+# with open("Book2.csv") as book:
+#     data = book.read();
+#     print(data)
+    # output:
+    # Area,Wether,Temprature
+    # Kotturu,Cloudy,29
+    # Ln peta ,Sunny,41
+    # Hiramandalam ,Sunny,38
+
 import csv
 
-with open("Book2.csv", newline="") as book:
-    rows = csv.reader(book)
-    temperatures = []
 
-    for row in rows:
-        if len(row) >= 3 and row[2] != "Temprature":
-            temperatures.append(row[2])
+# with open("Book2.csv") as book:
+#     data = csv.reader(book)
+#     for row in data:
+#         print(row[2])         # output:-['29', '41', '38']
 
-    print(temperatures)
+
+# with open("Book2.csv") as book:
+#     temprature = []
+#     data = csv.reader(book)
+#     for row in data:
+#         if row[2]!="Temprature":
+#             temprature.append(row[2])
+#     print(temprature)           # output:-['29', '41', '38']
+import pandas
+
+data = pandas.read_csv("Book2.csv")
+# print(data[Temprature])
+
+# data_dict = data.to_dict()
+# print(data_dict)
+
+# temp_list = data["Temprature"].to_list()
+# temp_village = data["Area"].to_list()
+# temp_wether = data["Wether"].to_list()
+# print(temp_list)    #[29, 41, 38]
+# print(temp_village) #['Kotturu', 'Ln peta ', 'Hiramandalam ']
+# print(temp_wether)  #['Cloudy', 'Sunny', 'Sunny']
+
+# print(data["Temprature"].mean()) 
+# # output : 36.0
+# print(data["Temprature"].max())
+# # output : 41
+# print(data["Temprature"].min())
+# # output : 29
+
+#get data in columns
+
+# print(data[data["Wether"]=="Sunny"])
+#output for the above line
+#             Area Wether  Temprature
+# 1       Ln peta   Sunny          41
+# 2  Hiramandalam   Sunny          38
+
+print(data[data["Temprature"]==data["Temprature"].max()])
