bcdf5d0b71e992ba438fc9965bfce019ba4c1bc3
nikhilkottakota2-dotcom <nikhilkottakota2@2gmail.com>
2026-08-28 19:05:56 +0530

    addition of reset specifications

--- pomodoro_timer/pomodoro_timer_project.py
+++ pomodoro_timer/pomodoro_timer_project.py
@@ -1,25 +1,32 @@
 from tkinter import *
 import math
 # ---------------- CONSTANTS ------------------------
-PINK = "#e2979c"
-RED = "#e7305b"
-GREEN = "#9bdeac"
-YELLOW = "#f7f5dd"
 FONT_NAME = "Courier"
 WORK_MIN = 25
 SHORT_BREAK_MIN = 5
 LONG_BREAK_MIN = 20
 #------------------TIMER RESET------------------------
 
+running = True
 def timer_reset():
+    global running
+    running = False
     canvas.itemconfig(timer_text,text="00:00")
+    
 
 #------------------TIMER MECHANISM--------------------
 def start_timer():
+
+    global running
+    running = True
     minutes = int(user_ent.get())
     count_down(minutes * 60)
 #------------------COUNTDOWN MECHANISM----------------
 def count_down(count):
+
+    global  running 
+    if not running:
+        return
 
     count_min = math.floor(count/60)
     count_sec = count % 60
@@ -29,6 +36,7 @@ def count_down(count):
     canvas.itemconfig(timer_text,text=f"{count_min}:{count_sec}")
     if count>0:
         window.after(1000,count_down,count-1)
+
 #------------------UI SETUP---------------------------
 
 window = Tk()
